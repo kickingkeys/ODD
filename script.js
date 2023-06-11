4b0b4a2a1e6c54475726fb7d59997f2490e6ab49
Ajith Ranka <ajithranka@users.noreply.github.com>
2023-06-11 13:20:44 +0100

    Fix carousel scroll

--- script.js
+++ script.js
@@ -1,48 +1,50 @@
-var carousel = document.querySelector('.carousel')
+imagesLoaded(document.querySelector(".carousel"), () => {
+  var carousel = document.querySelector('.carousel')
 
-var listA = document.querySelector('.carousel-list')
-var listB = listA.cloneNode(true)
+  var listA = document.querySelector('.carousel-list')
+  var listB = listA.cloneNode(true)
 
-var width = listA.offsetWidth
+  var width = listA.scrollWidth
 
-var xA = 0
-var xB = width
+  var xA = 0
+  var xB = width
 
-carousel.appendChild(listB)
-listB.style.left = width + 'px'
+  carousel.appendChild(listB)
+  listB.style.left = width + 'px'
 
-var speed = 1
-function move() {
-  xA -= speed
-  
-  if (Math.abs(xA) <= width) {
-    listA.style.left = `${xA}px`
-  } else {
-    xA = width
+  var speed = 1
+  function move() {
+    xA -= speed
+    
+    if (Math.abs(xA) <= width) {
+      listA.style.left = `${xA}px`
+    } else {
+      xA = width
+    }
+    
+    xB -= speed
+    
+    if (Math.abs(xB) <= width) {
+      listB.style.left = `${xB}px`
+    } else {
+      xB = width
+    }
+
+    if (isActive) {
+      window.requestAnimationFrame(move)
+    }
   }
-  
-  xB -= speed
-  
-  if (Math.abs(xB) <= width) {
-    listB.style.left = `${xB}px`
-  } else {
-    xB = width
-  }
 
-  if (isActive) {
+  var isActive = true
+
+  carousel.addEventListener("mouseenter", () => {
+    isActive = false
+  })
+
+  carousel.addEventListener("mouseleave", () => {
+    isActive = true
     window.requestAnimationFrame(move)
-  }
-}
+  })
 
-var isActive = true
-
-carousel.addEventListener("mouseenter", () => {
-  isActive = false
-})
-
-carousel.addEventListener("mouseleave", () => {
-  isActive = true
   window.requestAnimationFrame(move)
 })
-
-window.requestAnimationFrame(move)
